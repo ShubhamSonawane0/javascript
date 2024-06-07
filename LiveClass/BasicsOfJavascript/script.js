8df9b3e7c74d7c348f4e9bdcdce0860ba6d5e27d
ShubhamSonawane0 <163091443+ShubhamSonawane0@users.noreply.github.com>
2024-06-07 13:00:11 +0530

    basics

---
+++ LiveClass/BasicsOfJavascript/script.js
@@ -0,0 +1,30 @@
+console.log("hey there");
+
+
+// 1;
+
+//var math = 100;
+//var physics = 60;
+
+//var sum = physics + math;
+
+//console.log(sum);
+
+let math = 20;
+
+math = 80;
+
+console.log("math Marks:", math);
+
+let a = 99;
+
+a = 0;
+console.log("a:", a);
+
+let score = 350;
+
+var commentary1 = "hey india score is 300";
+let commentary2 = `hey india score is ${score}`;
+
+console.log(commentary1);
+console.log(commentary2);
