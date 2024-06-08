b5adf38213e264b3f5a5c7a467f91702f3a31837
ShubhamSonawane0 <163091443+ShubhamSonawane0@users.noreply.github.com>
2024-06-08 20:15:44 +0530

    declarion function

--- LiveClass/2.Function/script.js
+++ LiveClass/2.Function/script.js
@@ -1,17 +1,17 @@
 //-------- Function -------------
 
-
+/*
 function sum(a,b) {
     let c = a + b;
 
 }
 
 console.log(sum (1,2));
-
+*/
 
 //  ---------- function Declaration -----------
 
-function sum(a, b) {
+ /*function sum(a, b) {
     let c = a + b;
 
     return c;
@@ -23,4 +23,47 @@ const sum2 = function(a, b) {
 };
 
 console.log(sum(1, 2));
-console.log(sum2(99, 99));
+console.log(sum2(99, 99)); 
+*/
+// ------------ Function with default value ------------
+
+/* function sum(a,b) {
+    return a + b; 
+}
+
+console.log(sum(), "sum()");
+console.log(sum(1), 'sum(1)'); 
+console.log(sum(1,2), 'sum(1,2)');//3   
+console.log(sum(1,2,3,4,5,6,7,8,9), 'sum(1,2,3,4,5,6,7,8,9)'); //3
+ console.log(sum(5,5), 'sum');//10
+*/
+ // ------------ function returns ----------
+
+ /* function sum(a,b) {
+    let c = a + b;
+ }
+
+ sum(); // undefined
+
+ function abc() {
+    return 1;
+ }
+  
+ abc(); //1
+
+ function def() {
+    return 1;
+
+    return 2;
+ }
+
+ def();
+ */
+
+ function sum(a,b) {
+    return (a - b);
+    return (a + b);
+ }
+
+const result = sum(100, 80);
+console.log(result);
