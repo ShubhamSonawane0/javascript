2f336752a803c313d5991e7876d1cc1ecd9b1d40
ShubhamSonawane0 <163091443+ShubhamSonawane0@users.noreply.github.com>
2024-06-11 20:13:16 +0530

    Basics of functions

--- LiveClass/2.Function/script.js
+++ LiveClass/2.Function/script.js
@@ -60,7 +60,7 @@ console.log(sum(1,2,3,4,5,6,7,8,9), 'sum(1,2,3,4,5,6,7,8,9)'); //3
  def();
  */
 
- function sum(a,b) {
+ /*function sum(a,b) {
 
     console.log("bbbb");
     return (a + b);
@@ -68,3 +68,94 @@ console.log(sum(1,2,3,4,5,6,7,8,9), 'sum(1,2,3,4,5,6,7,8,9)'); //3
 
 const result = sum(100, 80);
 console.log(result); 
+*/
+  
+/*
+function adds(a = 0,b = 0) {
+return a + b
+}
+const ans1 = adds(80, 3);
+const ans2 = adds();
+const ans3 = adds(80);
+ 
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
+*/
+
+// ------------ Rest OPERATOR ------------
+
+/*
+function admissiontoschool(name, standard, ...info) {
+   const student ={
+      name: name,
+      std: standard,
+      address: info[0],
+   };
+   console.log(info, "info rest operator");
+
+   return student;
+
+}
+
+const shubhamobject = admissiontoschool("arun",9, "navi mumbai", "male", "father");
+
+console.log(shubhamobject); 
+*/
+
+//----------------- Pass by values vs pass by reference  ---------------------------
+/*
+function sum(a, b){
+   a = a + 10;
+   b = b + 10;
+
+   return a + b;
+}
+
+const res1 = sum(1, 2);
+console.log(res1, "res1"); //23
+*/
+
+/*
+let math = {
+   marks: 100,
+   grades: "A+"
+}
+let science = {
+   marks: 90,
+   grades: "A"
+}
+
+function summarks(obj1, obj2){
+   obj1.marks = 30;
+   obj2.marks = 40;
+
+   return obj1.marks + obj2.marks;
+
+}
+
+const result = summarks(math, science);
+
+console.log(result, "result"); //70 
+
+console.log(math.marks + science.marks, 'math + science') //70
+
+*/
+
+// --------------- Pure And Impure ---------------
+ 
+// THIS IS PURE FUNCTION
+function sum(a, b) {
+   let sum = a + b;
+
+   return sum;
+}
+
+// IMPURE FUNCTION 
+
+function sum(a, b) {
+
+   console.log("abcd");
+
+   return a + b;
+}
