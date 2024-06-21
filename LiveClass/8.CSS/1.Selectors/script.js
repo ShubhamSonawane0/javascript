1c151fd7a7e62ee09672755490403d09f2be90d2
ShubhamSonawane0 <163091443+ShubhamSonawane0@users.noreply.github.com>
2024-06-21 21:22:53 +0530

    basic

---
+++ LiveClass/8.CSS/1.Selectors/script.js
@@ -0,0 +1,22 @@
+
+
+
+// 3 way to give styling
+// 1. Inline styling
+// 2. styles styling
+// 3. External styling
+
+// --------------- CSS Selectors -----------------
+
+// ----------- Direct selector -------------
+
+// 1. Tag Selector
+// h1 {} 
+// p {}
+
+// 2. id selector
+// idname {}
+
+// 3. class selector
+
+// 
